b0824b211b6a0db0da1ec6fa8c6f3f4685c349dd
Colin Travis <colint@cybertravis.us>
2017-01-31 15:00:20 -0600

    Added Dino sprite

--- js/sketch.js
+++ js/sketch.js
@@ -16,8 +16,8 @@ var water;
 
 function preload(){
 
-  water = loadAnimation("../images/water-1.png","../images/water-4.png")
-  dino = loadAnimation("../images/dino-1.png","../images/dino-4.png")
+  water = loadAnimation("../images/water/water-1.png","../images/water/water-60.png")
+  dino = loadAnimation("../images/dino/dino_1.png","../images/dino/dino_60.png")
 
 }
 
@@ -49,9 +49,13 @@ function draw () {
   // image(waterSecond)
   // console.log(waterSecond);
   animation(water, windowWidth/2,windowHeight/1.2)
-  water.frameDelay = 60;
-  animation(dino,windowWidth/2,windowHeight/1.2)
-  dino.frameDelay = 3600;
+  water.frameDelay = 3600;
+  water.changeFrame(second());
+
+  animation(dino, windowWidth/2,windowHeight/1.2)
+  dino.frameDelay = 6000;
+
+  dino.changeFrame(minute());
 
 
   // fillToSecond = map(second(),0,60,0,height)
